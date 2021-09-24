43289f5b52c6a97d933a86a1945a5808f9292391
amd64fox <62529699+amd64fox@users.noreply.github.com>
2021-09-24 07:38:09 +0300

    Update LoaderSpot.ps1

--- LoaderSpot.ps1
+++ LoaderSpot.ps1
@@ -36,40 +36,40 @@ if ($version_spoti -notmatch '^\d.\d.\d{1,2}.\d{1,5}.[a-z][0-9a-z][0-9a-z][0-9a-
 
 
 
-if ($version_spoti -match '^\d.\d.\d{1,2}.\d{1,5}.[a-z][0-9a-z][0-9a-z][0-9a-z][0-9a-z][0-9a-z][0-9a-z][0-9a-z][0-9a-z]$') {
-    $before_enter = Read-Host -Prompt "Enter the number to stop at (e.g. 99)"
-    $numbers = 0
+    if ($version_spoti -match '^\d.\d.\d{1,2}.\d{1,5}.[a-z][0-9a-z][0-9a-z][0-9a-z][0-9a-z][0-9a-z][0-9a-z][0-9a-z][0-9a-z]$') {
+        $before_enter = Read-Host -Prompt "Enter the number to stop at (e.g. 99)"
+        $numbers = 0
 
-    "Search..."
-    While ($numbers -le $before_enter) {
+        "Search..."
+        While ($numbers -le $before_enter) {
     
-        $_URL = "https://upgrade.scdn.co/upgrade/client/win32-x86/spotify_installer-$version_spoti-$numbers.exe"
+            $_URL = "https://upgrade.scdn.co/upgrade/client/win32-x86/spotify_installer-$version_spoti-$numbers.exe"
         
 
-        try {
-            $request = [System.Net.WebRequest]::Create($_URL)
-            $response = $request.getResponse()
+            try {
+                $request = [System.Net.WebRequest]::Create($_URL)
+                $response = $request.getResponse()
 
-            if ($response.StatusCode -eq "200") {
-                $response.ResponseUri.OriginalString
-                $find_url += [System.Environment]::NewLine + $response.ResponseUri.OriginalString
-                $response.Close()
-            }
+                if ($response.StatusCode -eq "200") {
+                    $response.ResponseUri.OriginalString
+                    $find_url += [System.Environment]::NewLine + $response.ResponseUri.OriginalString
+                    $response.Close()
+                }
             
-        }
-        catch {
-            $numbers
-        }
+            }
+            catch {
+                $numbers
+            }
         
-        $numbers++
+            $numbers++
+        }
     }
-}
-else {
-Write-Host "Unsuccessful again, script stopped ..." -ForegroundColor Red 
-pause
-exit
+    else {
+        Write-Host "Unsuccessful again, script stopped ..." -ForegroundColor Red 
+        pause
+        exit
 
-}
+    }
 
 
 
@@ -77,12 +77,13 @@ exit
 write-host "`n"
 "Search completed"
 write-host "`n"
-if($find_url){
-"Found links :"
-$find_url}
-if(!($find_url)){
-"Nothing found, please increase your search range."}
+if ($find_url) {
+    "Found links :"
+    $find_url
+}
+if (!($find_url)) {
+    "Nothing found, please increase your search range."
+}
 write-host "`n"
 pause
 exit
-
