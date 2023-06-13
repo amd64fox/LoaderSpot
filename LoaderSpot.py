d28b01fde0630013b7ef7e9376d945beb25fe4b9
amd64fox <62529699+amd64fox@users.noreply.github.com>
2023-06-13 04:15:14 +0300

    Improve search algorithm
     - multithreading replaced with asynchrony
    - minor fixes in the code

--- LoaderSpot.py
+++ LoaderSpot.py
@@ -1,129 +1,153 @@
 import re
-import requests
-from concurrent.futures import ThreadPoolExecutor
+import aiohttp
+import asyncio
+from tqdm import tqdm
 
 
-def check_url(url, platform_name):
+async def check_url(session, url, platform_name):
     try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            if platform_name:
-                return response.url, platform_name
-            else:
-                return response.url
-        else:
-            print(f"\nInvalid link: {url}")
-        response.close()
-    except requests.exceptions.RequestException:
+        async with session.get(url) as response:
+            if response.status == 200:
+                if platform_name:
+                    return response.url, platform_name
+                else:
+                    return response.url
+    except aiohttp.ClientError:
         pass
     return None
 
 
-version_spoti = ""
-while not re.match(r"^\d+\.\d+\.\d+\.\d+\.g[0-9a-f]{8}$", version_spoti):
-    version_spoti = input("Spotify version, for example 1.1.68.632.g2b11de83: ")
+async def main(version_spoti=""):
+    if not version_spoti:
+        while not re.match(r"^\d+\.\d+\.\d+\.\d+\.g[0-9a-f]{8}$", version_spoti):
+            version_spoti = input("Spotify version, for example 1.1.68.632.g2b11de83: ")
 
-start_number = ""
-while not start_number.isdigit() or not 0 <= int(start_number) <= 20000:
-    start_number = input("Start search from: ")
+    start_number = ""
+    while not start_number.isdigit() or not 0 <= int(start_number) <= 20000:
+        start_number = input("Start search from: ")
 
-before_enter = ""
-while not before_enter.isdigit() or not 1 <= int(before_enter) <= 20000:
-    before_enter = input("End search at: ")
+    before_enter = ""
+    while not before_enter.isdigit() or not 1 <= int(before_enter) <= 20000:
+        before_enter = input("End search at: ")
 
-max_workers_req = ""
-while True:
-    max_workers_req = input("Number of threads: ")
-    try:
-        max_workers = int(max_workers_req)
-        if 1 <= max_workers <= 150:
-            break
+    numbers = int(start_number)
+    find_url = []
+
+    win32 = "https://upgrade.scdn.co/upgrade/client/win32-x86/spotify_installer-{version_spoti}-{numbers}.exe"
+    win_arm64 = "https://upgrade.scdn.co/upgrade/client/win32-arm64/spotify_installer-{version_spoti}-{numbers}.exe"
+    osx = "https://upgrade.scdn.co/upgrade/client/osx-x86_64/spotify-autoupdate-{version_spoti}-{numbers}.tbz"
+    osx_arm64 = "https://upgrade.scdn.co/upgrade/client/osx-arm64/spotify-autoupdate-{version_spoti}-{numbers}.tbz"
+
+    print("\nSelect the link type for the search:")
+    print("[1] WIN32")
+    print("[2] WIN-ARM64")
+    print("[3] OSX")
+    print("[4] OSX-ARM64")
+    print("[5] All platforms")
+
+    choices = []
+    while not choices:
+        choice = input("Enter the number(s): ")
+        choice_list = choice.split(",")
+        for c in choice_list:
+            c = c.strip()
+            if c == "1":
+                choices.append("1")
+            elif c == "2":
+                choices.append("2")
+            elif c == "3":
+                choices.append("3")
+            elif c == "4":
+                choices.append("4")
+            elif c == "5":
+                choices = ["1", "2", "3", "4"]
+                break
+            else:
+                print("Value should be in the range from 1 to 5")
+
+    print("\nSearching...\n")
+
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+
+        if "5" in choices:
+            url_templates = [win32, win_arm64, osx, osx_arm64]
+            platform_names = ["WIN32", "WIN-ARM64", "OSX", "OSX-ARM64"]
+            for url_template, platform_name in zip(url_templates, platform_names):
+                numbers = int(start_number)
+                while numbers <= int(before_enter):
+                    url = url_template.format(
+                        version_spoti=version_spoti, numbers=numbers
+                    )
+                    tasks.append(check_url(session, url, platform_name))
+                    numbers += 1
         else:
-            print("Value should be in the range from 1 to 150")
-    except ValueError:
-        print("Invalid value, please enter an integer")
+            for choice in choices:
+                if choice == "1":
+                    url_template = win32
+                    platform_name = "WIN32"
+                elif choice == "2":
+                    url_template = win_arm64
+                    platform_name = "WIN-ARM64"
+                elif choice == "3":
+                    url_template = osx
+                    platform_name = "OSX"
+                elif choice == "4":
+                    url_template = osx_arm64
+                    platform_name = "OSX-ARM64"
+                numbers = int(start_number)
+                while numbers <= int(before_enter):
+                    url = url_template.format(version_spoti=version_spoti, numbers=numbers)
+                    tasks.append(check_url(session, url, platform_name))
+                    numbers += 1
 
-numbers = int(start_number)
-find_url = []
+        results = []
 
-win32 = "https://upgrade.scdn.co/upgrade/client/win32-x86/spotify_installer-{version_spoti}-{numbers}.exe"
-win_arm64 = "https://upgrade.scdn.co/upgrade/client/win32-arm64/spotify_installer-{version_spoti}-{numbers}.exe"
-osx = "https://upgrade.scdn.co/upgrade/client/osx-x86_64/spotify-autoupdate-{version_spoti}-{numbers}.tbz"
-osx_arm64 = "https://upgrade.scdn.co/upgrade/client/osx-arm64/spotify-autoupdate-{version_spoti}-{numbers}.tbz"
+        with tqdm(total=len(tasks)) as pbar:
+            for task in asyncio.as_completed(tasks):
+                result = await task
+                if result is not None:
+                    find_url.append(result)
+                pbar.update(1)
 
-print("Select the link type for the search:")
-print("[1] WIN32")
-print("[2] WIN-ARM64")
-print("[3] OSX")
-print("[4] OSX-ARM64")
-print("[5] ALL")
+    print("\nSearch completed:\n")
+    if find_url:
+        platform_urls = {}
+        for item in find_url:
+            if isinstance(item, tuple):
+                url, platform_name = item
+                if platform_name not in platform_urls:
+                    platform_urls[platform_name] = []
+                platform_urls[platform_name].append(url)
+            else:
+                if "Unknown" not in platform_urls:
+                    platform_urls["Unknown"] = []
+                platform_urls["Unknown"].append(item)
 
-choice = None
-while choice not in ["1", "2", "3", "4", "5"]:
-    choice = input("Enter the number: ")
+        for platform_name, urls in platform_urls.items():
+            print(f"{platform_name}:")
+            for url in urls:
+                print(url)
+            print()
+    else:
+        print("Nothing found, consider increasing the search range\n")
+
+    choice = input("Choose an option:\n"
+                   "[1] Perform the search with a new version\n"
+                   "[2] Perform the search again with the same version\n"
+                   "[3] Exit\n"
+                   "Enter the number: ")
 
     if choice == "1":
-        url_template = win32
-        platform_name = "WIN32"
+        print("\n")
+        await main()
     elif choice == "2":
-        url_template = win_arm64
-        platform_name = "WIN-ARM64"
+        print("\n")
+        print(f"Search version: {version_spoti}")
+        await main(version_spoti)
     elif choice == "3":
-        url_template = osx
-        platform_name = "OSX"
-    elif choice == "4":
-        url_template = osx_arm64
-        platform_name = "OSX-ARM64"
-    elif choice == "5":
-        url_templates = [win32, win_arm64, osx, osx_arm64]
-        platform_names = ["WIN32", "WIN-ARM64", "OSX", "OSX-ARM64"]
+        return
     else:
-        print("Value should be in the range from 1 to 5")
+        print("Invalid choice. Exiting the code.")
 
-print("Searching...")
-
-with ThreadPoolExecutor(max_workers=max_workers) as executor:
-    tasks = []
-
-    if choice == "5":
-        for url_template, platform_name in zip(url_templates, platform_names):
-            numbers = int(start_number)
-            while numbers <= int(before_enter):
-                url = url_template.format(version_spoti=version_spoti, numbers=numbers)
-                tasks.append(executor.submit(check_url, url, platform_name))
-                numbers += 1
-    else:
-        while numbers <= int(before_enter):
-            url = url_template.format(version_spoti=version_spoti, numbers=numbers)
-            tasks.append(executor.submit(check_url, url, platform_name))
-            numbers += 1
-
-    for task in tasks:
-        result = task.result()
-        if result is not None:
-            find_url.append(result)
-
-print("\nSearch completed.\n")
-if find_url:
-    platform_urls = {}
-    for item in find_url:
-        if isinstance(item, tuple):
-            url, platform_name = item
-            if platform_name not in platform_urls:
-                platform_urls[platform_name] = []
-            platform_urls[platform_name].append(url)
-        else:
-            if "Unknown" not in platform_urls:
-                platform_urls["Unknown"] = []
-            platform_urls["Unknown"].append(item)
-
-    for platform_name, urls in platform_urls.items():
-        print(f"{platform_name}:")
-        for url in urls:
-            print(url)
-        print()
-else:
-    print("Nothing found, consider increasing the search range.")
-
-print("\n")
-input("Press Enter to exit")
+asyncio.run(main())
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
     <a href="https://docs.google.com/spreadsheets/d/1wztO1L4zvNykBRw7X4jxP8pvo11oQjT0O5DvZ_-S4Ok/edit#gid=0"><img src="https://img.shields.io/badge/Excel%20table--brightgreen.svg?style=flat&logo=microsoftexcel&label=Excel table"></a>
 </p>
 <h1 align="center">LoaderSpot</h1>
-<h2 align="center">A tool for downloading the full Spotify Desktop client.</h2>
+<h2 align="center">Tool for finding full Spotify installers.</h2>
 <p align="center">
     <h3 align="center">I also created an updatable document, with all the links I found. You can see it <a href="https://docs.google.com/spreadsheets/d/1wztO1L4zvNykBRw7X4jxP8pvo11oQjT0O5DvZ_-S4Ok/edit#gid=0">here</a></h3>
 </p>
